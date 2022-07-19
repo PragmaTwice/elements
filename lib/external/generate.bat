e57ed2b42f238f54e90dff597d1341996f4767a6
PragmaTwice <twice.mliu@gmail.com>
2022-07-19 15:35:58 +0800

    fix typo

--- lib/external/generate.bat
+++ lib/external/generate.bat
@@ -2,6 +2,6 @@ git clone git://anongit.freedesktop.org/git/cairo
 cd cairo
 git checkout 1.17.6
 
-meson setup build64 --buildtype release -D b_vscrt=mt --auto-features disabled -D fontconfig=enabled -D freetype=enabled -D png=enabled
-meson compile -C build64
-meson install -C build64 --destdir install
+meson setup meson-build --buildtype release -D b_vscrt=mt --auto-features disabled -D fontconfig=enabled -D freetype=enabled -D png=enabled
+meson compile -C meson-build
+meson install -C meson-build --destdir install

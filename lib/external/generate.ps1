dce0f63867ba884e5182e6599682d0f1cdd066b0
PragmaTwice <twice.mliu@gmail.com>
2022-07-19 20:07:02 +0800

    fix ps

--- lib/external/generate.ps1
+++ lib/external/generate.ps1
@@ -3,7 +3,7 @@ cd cairo
 git checkout 1.17.6
 
 meson setup meson-build --backend vs2022 --buildtype release -D b_vscrt=mt --auto-features disabled -D fontconfig=enabled -D freetype=enabled -D png=enabled
-if (meson compile -C meson-build) {}
+(meson compile -C meson-build) -or $true
 echo $null >> meson-build/subprojects/freetype/libfreetype.a
-if (meson install -C meson-build --destdir install) {}
+(meson install -C meson-build --destdir install) -or $true
 echo success

a50b695e0a324d934d8adf4af207120a6328b77e
PragmaTwice <twice.mliu@gmail.com>
2022-07-19 17:46:45 +0800

    fix missing file

--- lib/external/generate.ps1
+++ lib/external/generate.ps1
@@ -4,4 +4,5 @@ git checkout 1.17.6
 
 meson setup meson-build --backend vs2022 --buildtype release -D b_vscrt=mt --auto-features disabled -D fontconfig=enabled -D freetype=enabled -D png=enabled
 (meson compile -C meson-build)
+echo $null >> meson-build/subprojects/freetype/libfreetype.a
 (meson install -C meson-build --destdir install)
